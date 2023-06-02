8de4788385913a2c53064a8418d6bab79868692f
Jan Christiaan Zwier <6506941+JCZwier@users.noreply.github.com>
2023-06-02 17:30:48 +0200

    Added co-citation counting function

--- prices_model.py
+++ prices_model.py
@@ -1,15 +1,22 @@
 import random
+import itertools
 import networkx as nx
-
-import matplotlib.pyplot as plt     # !!! REPLACE
+import matplotlib.pyplot as plt     # !!! replace?
+from collections import Counter
 
 n = 5000			# total number of papers in the network
 c = 30				# number of papers that a paper cites on average
 a = 1				# ground state (paper initially only cites itself)
 p_pref = c / (a+c)	# probability of citing a paper based on preferential attachment (1-p is the probability of citing a random paper)
+
+def countCoCitations(dg, n):
+	co_citations = []
 	
-def countCoCitations(dg):
-	pass
+	for i in range(n):
+		for subset in itertools.combinations(dg.successors(i), 2):
+			co_citations.append(subset)
+			
+	return Counter(tuple(sorted(t)) for t in co_citations)
 	
 def createCitationNetwork(n):	# with preferential attachment
 	dg = nx.DiGraph()
@@ -22,25 +29,37 @@ def createCitationNetwork(n):	# with preferential attachment
 		elif i == 1:
 			dg.add_edge(1, 0)
 		elif i > 1:
-			citations = []
 			for j in range(min(dg.number_of_nodes()-1, c)):
-				while citations[j] == None or citations[j] == i or citations[j] in citations:	# !!! use two while statements?
-					if random.uniform(0,1) <= p_pref:	# citing a paper based on preferential attachment
-						citations[j] = random.choices(list(dg.nodes), [val+1 for (node, val) in dg.in_degree()])[0]	# +1 because a paper always cites itself
+				# !!! maybe do this differently
+				cited_paper = None
+				p = random.uniform(0, 1)
+				while cited_paper == None or cited_paper == i or cited_paper in dg.successors(i):
+					if p <= p_pref:	# citing a paper based on preferential attachment
+						cited_paper = random.choices(list(dg.nodes), [val+1 for (node, val) in dg.in_degree()])[0]	# +1 because a paper always cites itself
 					else:	# citing a random paper
-						citations[j] = random.choice(list(dg.nodes))
-			dg.add_edges_from(citations)
+						cited_paper = random.choice(list(dg.nodes))
+				dg.add_edge(i, cited_paper)
 				
 	return dg
 
 cn = createCitationNetwork(n)
+cc_count = countCoCitations(cn, n)
+
+# ~ for value, count in cc_count.most_common(1000):
+	# ~ print(value, count)
 
 print(sorted(cn.in_degree, key=lambda x: x[1], reverse=True))
 
+print("Amount of co-citation pairs: ", len(cc_count))
+
 fig, ax = plt.subplots()
-ax.plot(sorted((val for (node, val) in cn.in_degree), reverse=True))
+ax.plot(sorted((val for (key, val) in cc_count.items()), reverse=True))
 plt.show()
 
+# ~ fig, ax = plt.subplots()
+# ~ ax.plot(sorted((val for (node, val) in cn.in_degree), reverse=True))
+# ~ plt.show()
+
 # ~ plt.figure(figsize=(100,100))
-# ~ nx.draw_networkx(dg, pos = nx.random_layout(cn))
+# ~ nx.draw_networkx(cn, pos = nx.random_layout(cn))
 # ~ plt.savefig("graph.png")
