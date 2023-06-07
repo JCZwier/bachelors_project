5d5fcf0be0ce532d46bc6f5a549a33eda61e2df9
Jan Christiaan Zwier <6506941+JCZwier@users.noreply.github.com>
2023-06-07 18:26:24 +0200

    Improved the randomization function and created a function to compute z-scores

--- citation_network.py
+++ citation_network.py
@@ -6,24 +6,62 @@ import networkx as nx
 import matplotlib.pyplot as plt     # !!! replace?
 from collections import Counter
 
-n = 5000			# total number of papers in the network
+n = 250				# total number of papers in the network
 c = 30				# number of papers that a paper cites on average
 a = 1				# ground state (paper initially only cites itself)
 p_pref = c / (a+c)	# probability of citing a paper based on preferential attachment (1-p is the probability of citing a random paper)
-
-def countCoCitations(dg, n):
+n_null = 10			# number of null models used for the computation of the z-scores	
+	
+def countCoCitations(dg):
 	co_citations = []
 	
-	for i in range(n):
+	for i in range(dg.number_of_nodes()):
 		for subset in itertools.combinations(dg.successors(i), 2):
 			co_citations.append(subset)
 			
 	return Counter(tuple(sorted(t)) for t in co_citations)
 	
-def createCitationNetwork(n):	# with preferential attachment
+def createNullModels(dg, n_instances):
+	null_models = []
+	
+	for i in range(n_instances):
+		null_models.append(nx.directed_configuration_model([val for (node, val) in dg.in_degree], [val for (node, val) in dg.out_degree]))
+		
+	return null_models
+	
+def computeZScores(cc_count, cc_counts_null, n_null):
+	cc_counts_null_distributions = []
+	z_scores = []
+	
+	for i in range(len(cc_count)):
+		pair = list(cc_count.keys())[i]
+		value = list(cc_count.values())[i]
+		cc_counts_null_distributions.append([])
+		
+		for j in range(n_null):
+			if pair in cc_counts_null[j].keys():
+				cc_counts_null_distributions[i].append(cc_counts_null[j].get(pair))
+			else:
+				cc_counts_null_distributions[i].append(0)
+				
+		if not np.any(cc_counts_null_distributions[i]):
+			z = None
+		else:
+			exp = np.mean(cc_counts_null_distributions[i])		
+			std = np.std(cc_counts_null_distributions[i])
+		
+			z = (float(value) - exp) / std
+			
+		z_scores.append(z)
+		# ~ print(cc_counts_null_distributions[i])
+		
+	print(z_scores)
+	return z_scores
+	
+def createCitationNetwork(n_nodes):	# with preferential attachment
 	dg = nx.DiGraph()
 	
-	for i in range(n):
+	for i in range(n_nodes):
 		dg.add_node(i)
 		
 		if i == 0:
@@ -47,12 +85,17 @@ def createCitationNetwork(n):	# with preferential attachment
 start = time.time()
 
 cn = createCitationNetwork(n)
-cc_count = countCoCitations(cn, n)
-randomized_cn = nx.directed_configuration_model([val for (node, val) in cn.in_degree], [val for (node, val) in cn.out_degree])
-randomized_cc_count = countCoCitations(randomized_cn, n)
+cc_count = countCoCitations(cn)
 
 print("Amount of co-citation pairs in original network: ", len(cc_count))
-print("Amount of co-citation pairs in randomized network: ", len(randomized_cc_count))
+
+null_models = createNullModels(cn, n_null)
+cc_counts_null = []
+
+for i in range(n_null):
+	cc_counts_null.append(countCoCitations(null_models[i]))
+	
+z_scores = computeZScores(cc_count, cc_counts_null, n_null)
 
 end = time.time()
 
