fa149d29f0c8a728e573bf94da7200c326f36a56
Jan Christiaan Zwier <6506941+JCZwier@users.noreply.github.com>
2023-06-12 21:53:04 +0200

    Added function for assigning z-score distributions to nodes

--- citation_network.py
+++ citation_network.py
@@ -6,20 +6,20 @@ import networkx as nx
 import matplotlib.pyplot as plt     # !!! replace?
 from collections import Counter
 
-n = 10000			# total number of papers in the network
+n = 500				# total number of papers in the network
 c = 30				# number of papers that a paper cites on average
 a = 1				# ground state (paper initially only cites itself)
 p_pref = c / (a+c)	# probability of citing a paper based on preferential attachment (1-p is the probability of citing a random paper)
-n_null = 10			# number of null models used for the computation of the z-scores	
+n_null = 10			# number of null models used for the computation of z-scores
+
+def countCoCitationPairs(dg):
+	cc_pairs_count = []		# !!! list might not be necessary
 	
-def countCoCitations(dg):
-	co_citations = []
-	
-	for i in range(dg.number_of_nodes()):
-		for subset in itertools.combinations(dg.successors(i), 2):
-			co_citations.append(subset)
+	for node in dg.nodes():
+		for subset in itertools.combinations(dg.successors(node), 2):
+			cc_pairs_count.append(subset)
 			
-	return Counter(tuple(sorted(t)) for t in co_citations)
+	return Counter(tuple(sorted(t)) for t in cc_pairs_count)
 	
 def createNullModels(dg, n_instances):
 	null_models = []
@@ -29,9 +29,9 @@ def createNullModels(dg, n_instances):
 		
 	return null_models
 	
-def computeZScores(cc_count, cc_counts_null, n_null):
+def computeZScores(dg, cc_count, cc_counts_null, n_null):
 	cc_counts_null_distributions = []
-	z_scores = []
+	z_scores = {}
 	
 	for i in range(len(cc_count)):
 		pair = list(cc_count.keys())[i]
@@ -53,9 +53,15 @@ def computeZScores(cc_count, cc_counts_null, n_null):
 		else:	
 			z = (value - exp) / std
 			
-		z_scores.append(z)
-		
+		z_scores[pair] = z
+	
 	return z_scores
+	
+def createZDistributions(dg, z_scores):
+	for node in dg.nodes():
+		dg.nodes[node]["z_score_dist"] = []
+		for subset in itertools.combinations(dg.successors(node), 2):
+			dg.nodes[node]["z_score_dist"].append(z_scores.get(subset))
 	
 def createCitationNetwork(n_nodes):	# with preferential attachment
 	dg = nx.DiGraph()
@@ -73,7 +79,7 @@ def createCitationNetwork(n_nodes):	# with preferential attachment
 				cited_paper = None
 				p = random.uniform(0, 1)
 				while cited_paper == None or cited_paper == i or cited_paper in dg.successors(i):
-					if p <= p_pref:	# citing a paper based on preferential attachment
+					if p <= p_pref:		# citing a paper based on preferential attachment
 						cited_paper = random.choices(list(dg.nodes), [val+1 for (node, val) in dg.in_degree()])[0]	# +1 because a paper always cites itself
 					else:	# citing a random paper
 						cited_paper = random.choice(list(dg.nodes))
@@ -84,34 +90,54 @@ def createCitationNetwork(n_nodes):	# with preferential attachment
 start = time.time()
 
 cn = createCitationNetwork(n)
-cc_count = countCoCitations(cn)
-
-print("Amount of co-citation pairs in original network: ", len(cc_count))
+cc_pairs_count = countCoCitationPairs(cn)
 
 null_models = createNullModels(cn, n_null)
-cc_counts_null = []
 
+cc_pairs_counts_null = []
 for i in range(n_null):
-	cc_counts_null.append(countCoCitations(null_models[i]))
+	cc_pairs_counts_null.append(countCoCitationPairs(null_models[i]))
 	
-z_scores = computeZScores(cc_count, cc_counts_null, n_null)
+z_scores = computeZScores(cn, cc_pairs_count, cc_pairs_counts_null, n_null)
+createZDistributions(cn, z_scores)
+
+# !!! temporary measure to remove the None values from the z-scores plot
+while(None in z_scores):
+    z_scores.remove(None)
 
 end = time.time()
 
 print("Time elapsed: ", end - start)
 
+z_medians = []
+for node in cn.nodes():
+	if cn.nodes[node]["z_score_dist"] and not all(z is None for z in cn.nodes[node]["z_score_dist"]):
+		z_medians.append(np.median([z for z in cn.nodes[node]["z_score_dist"] if z is not None]))
+
+# Plot z-scores of all co-citation pairs
 # ~ fig, ax = plt.subplots()
-# ~ ax.plot(sorted((val for (key, val) in cc_count.items()), reverse=True))
+# ~ ax.plot(sorted([z for z in z_scores.values() if z is not None]))
 # ~ plt.show()
 
+# Plot median z-distributions of all nodes
+# ~ fig, ax = plt.subplots()
+# ~ ax.plot(sorted(z_medians))
+# ~ plt.show()
+
+# Plot frequency of co-citation pairs
+# ~ fig, ax = plt.subplots()
+# ~ ax.plot(sorted((val for (key, val) in cc_pairs_count.items()), reverse=True))
+# ~ plt.show()
+
+# Plot amount of indegrees of all nodes
 # ~ fig, ax = plt.subplots()
 # ~ ax.plot(sorted((val for (node, val) in cn.in_degree), reverse=True))
 # ~ plt.show()
 
-# ~ plt.figure(figsize=(100,100))
+# Draw the citation network and save it as an image file
 # ~ nx.draw_networkx(cn, pos = nx.random_layout(cn))
 # ~ plt.savefig("citation_network.png")
 
-# ~ plt.figure(figsize=(100,100))
-# ~ nx.draw_networkx(randomized_cn, pos = nx.random_layout(randomized_cn))
+# Draw the first null model and save it as an image file
+# ~ nx.draw_networkx(null_models[0], pos = nx.random_layout(null_models[0]))
 # ~ plt.savefig("citation_network_randomized.png")
